30d8a1b9ea46e07d4397c2b95bc60c4be15fd50a
leonardomatumona <leonardomatumona@gmail.com>
2020-05-15 02:21:55 -0300

    nova aplicaçao

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
 const input = document.querySelector("#new-task")
 const button = document.querySelector("#add-task-button")
-const lista = document.querySelector(".task-list")
+const list = document.querySelector(".task-list")
 
 function adicionar(event) {
+    if(input.value===""){
+    return;  
 
+    }
 
-    const criarItem = document.createElement("li")
-    criarItem.textContent = input.value
-    criarItem.classList.add("task-list__item")
+    const criarItem = newItem(input.value)
     lista.appendChild(criarItem)
-    input.value = ""
+    input.value=""
+    input.focus()
+}
+
+function newItem(recebertexto){
+    const criarItem= document.createElement("li")
+    criarItem.classList.add("task-list__item")
+    criarItem.innerText= recebertexto
+    criarItem.appendChild(createbutton) 
+    return newItem   
+}
+
+function createbutton (){
+    const botao = document.createElement("button")
+    botao.type ="button"
+    botao.classList.add("task-list_action")
+    botao.innerHTML="<i class=\"fas fa-check-square\"></i>"
+    botao.addEventListener("click",drawtext)
+    return botao
+}
+
+function drawtext(event){
+    const botao= event.currentTarget;
+    const item= botao.parentNode
+
+    item.classList.remove("task-list__item")
+    item.classList.add("task-list__item--done")
+
 
 }
 
