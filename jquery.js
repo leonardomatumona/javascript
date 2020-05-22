d0c32ecd2f5973c8808f31672bd3b2c811f062ca
leonardomatumona <leonardomatumona@gmail.com>
2020-05-22 00:57:39 -0300

    atualização

--- jquery.js
+++ jquery.js
@@ -1,18 +1,40 @@
 $(function () {
     $("#mostrar").click(mostrar)
     $("#esconder").click(esconder)
-    $("img").hover
+    $("#img").hover(function(){
+        $(this).fadeTo(0,0.5)
+    })
+    $("#img").mouseleave(function(){
+        $(this).fadeTo(0,1)
+    })
+    $("h1").hover(function(){
+        $(this).addClass("destaque")
+    })
+    $("h1").mouseleave(function(){
+        $(this).removeClass("destaque")
+    })
     $("#p1").hover(aplicarDestaque)
-    $("#p1").hover( tirarDestaque)
-
-
-
+    $("#p1").mouseleave(tirarDestaque)
 })
+
 function mostrar() {
     $("#p1").fadeIn(1000)
 
 }
 function esconder(){
-    $("#p1").fadeTo(3000; 0.5)
+    $("#p1").fadeOut(3000)
 }
+
+function aplicarDestaque(){
+    $("#p1").addClass("destaque")
+   
+}
+
+function tirarDestaque(){
+    $("#p1").removeClass("destaque")
+
+}
+
+
+
 
